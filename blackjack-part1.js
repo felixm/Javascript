2ee6cd1c8ddabb37ba9f34fdf571b2d6367b6b50
felixm <frm6@njit.edu>
2012-06-07 01:35:52 +0000

    first commit

---
+++ blackjack-part1.js
@@ -0,0 +1,35 @@
+// Our deal function will return a random card
+var deal = function() {
+  card = Math.floor(Math.random()*52+1);
+  return card%13;
+};
+
+// Deal out our first hand
+var card1 = deal();
+var card2 = deal();
+
+// This function takes a card as a parameter and returns the value
+// of that card
+var getValue = function(card) {
+  // if its a face card, number should be set to 10        
+    if(card % 13 === 0 || card % 13 === 11 || card % 13 === 12){
+    return (card=10);
+    }
+  // What if it's an ace?
+ else if (card % 13 === 1)
+{
+    return (card=11);
+}
+  // Otherwise, we just want its number value
+ else 
+  return (card % 13); 
+};
+
+// Score the hand
+function score() {
+  return getValue(card1) + getValue(card2);
+}
+
+console.log("You have cards " + card1 + " and " + card2 +
+        " for a score of " + score(card1, card2));
+
